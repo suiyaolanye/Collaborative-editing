07a26484d9124ad6163f6838b698714139085f83
lanxi <1832246302@qq.com>
2016-08-28 10:41:08 +0800

    congif environment

--- mnote-search/Test.java
+++ mnote-search/Test.java
@@ -1,14 +1,18 @@
 package com.jiurong.mnote.search;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;  
+import org.elasticsearch.client.Client;  
+import org.elasticsearch.node.Node;
 
 public class Test {
-	public static void mian(String[] args) {
+	public static void main(String[] args) {
+//		System.out.println("aaa");
 		
+		Node node = nodeBuilder().node();
+//		Node node = nodeBuilder().clusterName("escluster2").client(true).node();  
+		Client client = node.client(); 
 		
-		Analyzer analyzer = new StandardAnalyzer();
+		node.close();
+			
 	}
 }
-
-
