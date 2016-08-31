51e28e004cbe36121048c019b89363048c805186
suiyaolanye <1832246302@qq.com>
2016-08-31 12:10:19 +0800

    test

--- mnote-search/Test.java
+++ mnote-search/Test.java
@@ -34,48 +34,34 @@ public class Test {
 	}
 	
 	public static void main(String[] args) {
-		String documentId = uuid();
-		String paragraphId = uuid();
+//		String documentId = uuid();
+//		String paragraphId = uuid();
+		String documentId = "c";
+		String paragraphId = "12";
 		String text = "久荣科技";
 		
 		BasicConfigurator.configure();
-		
-//		Node node = nodeBuilder().node();
-//		Node node = nodeBuilder().clusterName("yourclustername").node();
-//		Node node = nodeBuilder().client(true).node();
-//		Node node = nodeBuilder().clusterName("yourclustername").client(true).node();  
-//		Client client = node.client(); 
-		
-		
-		
-		try {
-			
-//			Settings settings = Settings.settingsBuilder()
-//			        .put("client.transport.sniff", true).build();
-//			TransportClient client = TransportClient.builder().settings(settings).build();
-			
-			 
+
+		try {	 
 			Settings.Builder settings = Settings.builder().put();  
 			Client client = TransportClient.builder().settings(settings).build()  
 			                .addTransportAddress(new InetSocketTransportAddress(  
 			                        InetAddress.getByName("localhost"), Integer.parseInt("9300")));  
 			
-			 IndexResponse response = client.prepareIndex("comment_index", "comment_ugc", "comment_123674")  
-				    .setSource( XContentFactory.jsonBuilder()  
-				    .startObject()  
-				      .field("author", "569874")  
-				      .field("author_name", "riching")  
-				      .field("mark", 232)  
-				      .field("body", "aa北京不错，但是人太多了")  
-				      .field("createDate", "20130801175520")  
-				      .field("valid", true)  
-				    .endObject())  
-				    .setTTL(8000)  
-				    .execute().actionGet();
-		
-			System.out.println(response.getId());  
+//			 IndexResponse response = client.prepareIndex(documentId, paragraphId)
+//				    .setSource( XContentFactory.jsonBuilder()  
+//				    .startObject()  
+//				      .field("author", "569874")  
+//				      .field("body", "aa北京不错，但是人太多了")  
+//				      .field("createDate", new Date())  
+//				      .field("isArray",false)
+//				    .endObject())  
+//				    .setTTL(8000)  
+//				    .execute().actionGet();
+//		
+//			System.out.println(response.getId());  
 			
-			SearchRequestBuilder builder = client.prepareSearch("comment_index").setTypes("comment_ugc").setSearchType(SearchType.DEFAULT).setFrom(0).setSize(100);  
+			SearchRequestBuilder builder = client.prepareSearch(documentId).setTypes().setSearchType(SearchType.DEFAULT).setFrom(0).setSize(100);  
 			BoolQueryBuilder qb = QueryBuilders.boolQuery().must(new   QueryStringQueryBuilder("北京").field("body"))  
 			    .should(new QueryStringQueryBuilder("但是").field("body"));  
 			builder.setQuery(qb);  
@@ -87,20 +73,46 @@ public class Test {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();	
-		}
-		
-		
-		
-//		node.close();
-		
-	
-//		Settings settings = Settings.settingsBuilder()
-//		        .put("cluster.name", "myClusterName").build();
-//		Client client = TransportClient.builder().settings(settings).build();
-//		client.close();
-		
-		
-		
+		}	
 		
 	}
 }
+
+
+
+//Node node = nodeBuilder().node();
+//Node node = nodeBuilder().clusterName("yourclustername").node();
+//Node node = nodeBuilder().client(true).node();
+//Node node = nodeBuilder().clusterName("yourclustername").client(true).node();  
+//Client client = node.client(); 
+
+
+//Settings.Builder settings = Settings.builder().put();  
+//Client client = TransportClient.builder().settings(settings).build()  
+//                .addTransportAddress(new InetSocketTransportAddress(  
+//                        InetAddress.getByName("localhost"), Integer.parseInt("9300")));  
+//
+// IndexResponse response = client.prepareIndex("comment_index", "comment_ugc", "comment_123674")  
+//	    .setSource( XContentFactory.jsonBuilder()  
+//	    .startObject()  
+//	      .field("author", "569874")  
+//	      .field("author_name", "riching")  
+//	      .field("mark", 232)  
+//	      .field("body", "aa北京不错，但是人太多了")  
+//	      .field("createDate", "20130801175520")  
+//	      .field("valid", true)  
+//	    .endObject())  
+//	    .setTTL(8000)  
+//	    .execute().actionGet();
+//
+//System.out.println(response.getId());  
+//
+//SearchRequestBuilder builder = client.prepareSearch("comment_index").setTypes("comment_ugc").setSearchType(SearchType.DEFAULT).setFrom(0).setSize(100);  
+//BoolQueryBuilder qb = QueryBuilders.boolQuery().must(new   QueryStringQueryBuilder("北京").field("body"))  
+//    .should(new QueryStringQueryBuilder("但是").field("body"));  
+//builder.setQuery(qb);  
+//SearchResponse responseSearch = builder.execute().actionGet();  
+//System.out.println("  " + responseSearch);  
+//System.out.println(responseSearch.getHits().getTotalHits()); 
+//
+//client.close();
